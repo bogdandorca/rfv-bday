c3962c795db6dd0aa4eeb23f92e917063e255a77
Bogdan Dorca <bogdan.dorca@xoomworks.com>
2020-07-06 15:09:56 +0300

    Fix hours

--- public/script.js
+++ public/script.js
@@ -27,7 +27,7 @@
         const total = Date.parse(targetDate) - Date.parse(new Date());
         const seconds = Math.floor( (total/1000) % 60 );
         const minutes = Math.floor( (total/1000/60) % 60 );
-        const hours = Math.floor( (total/(1000*60*60)) % 24 );
+        const hours = Math.floor( (total/(1000*60*60)) % 24 ) - 3;
         const days = Math.floor( total/(1000*60*60*24) );
 
         return {
